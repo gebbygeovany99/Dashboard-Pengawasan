be70aa32bed82224f3e01d4d1d64df2d2f3eaf87
Gebby Geovany <92412757+gebbygeovany99@users.noreply.github.com>
2025-11-19 14:18:26 +0700

    Add framework

---
+++ src/Components/CardDropZone.js
@@ -0,0 +1,19 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+export default function BasicCard() {
+  return (
+    <Card sx={{ minWidth: 275, mt: 5 }}>
+      <CardContent>
+        
+      </CardContent>
+      <CardActions>
+      </CardActions>
+    </Card>
+  );
+}
